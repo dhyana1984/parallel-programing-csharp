6e1b8123bc081b44d2b8b86fe3daeebbbdf4f766
yxiong <yxiong@fnz.com>
2022-07-29 01:25:14 +0800

    Lazy

---
+++ ParallelPrograming/ParallelPrograming/ch07/LazyInitializerSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelPrograming.ch07
+{
+    public class LazyInitializerSample
+    {
+        static Data _data;
+        static bool _initiallized;
+        static object _locker = new object();
+        public static void UseLazyInitializer()
+        {
+            Parallel.For(0, 10, i =>
+            {
+                Console.WriteLine($"Task with id {Task.CurrentId}");
+                LazyInitializer.EnsureInitialized(ref _data, ref _initiallized, ref _locker, FetchData);
+            });
+
+            Console.ReadLine();
+        }
+
+        private static Data FetchData()
+        {
+            Console.WriteLine($"Task with id {Task.CurrentId} is Initializing data");
+            return new Data();
+        }
+    }
+}
+
---
+++ ParallelPrograming/ParallelPrograming/ch07/LazySample.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace ParallelPrograming.ch07
+{
+    public class Data
+    {
+
+    }
+
+    public class DataWrapper
+    {
+        public DataWrapper()
+        {
+            CachedData = GetDataFromDatabase();
+            Console.WriteLine("Object initialied");
+        }
+
+        public Data CachedData { get; set; }
+        private Data GetDataFromDatabase()
+        {
+            // simulate delay
+            Thread.Sleep(1000);
+            return new Data();
+        }
+    }
+
+    public class LazySample
+    {
+        public LazySample()
+        {
+        }
+
+        public static void UseLazyByConstructor()
+        {
+            Console.WriteLine("Creating Lazy object");
+            // lazy call DataWrapper constructor to get data
+            Lazy<DataWrapper> lazyWrapper = new Lazy<DataWrapper>();
+            Console.WriteLine("Lazy Object created");
+            Console.WriteLine("Now we want to access data");
+            var data = lazyWrapper.Value.CachedData;
+            Console.WriteLine("Finising up");
+        }
+
+        public static void UseLazyByDelegate()
+        {
+
+            Console.WriteLine("Creating Lazy object");
+            // Lazy call the method to get data
+            Lazy<Data> lazyDataWrapper = new Lazy<Data>(GetDataFromDatabase);
+            Console.WriteLine("Lazy Object created");
+            Console.WriteLine("Now we want to access data");
+            var data = lazyDataWrapper.Value;
+            Console.WriteLine("Finising up");
+        }
+
+        private static Data GetDataFromDatabase()
+        {
+            Console.WriteLine("Fetching Data");
+            // simulate delay
+            Thread.Sleep(1000);
+            return new Data();
+        }
+
+    }
+}
+
